0ec224b85608d13f039cfa96ee47216326b691ff
dddw001 <2410656679@qq.com>
2018-04-14 23:05:35 +0800

    a pos project just part

---
+++ pos-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <left-nav></left-nav>
+    <div class="main">
+      <router-view/>      
+    </div>
+  </div>
+</template>
+
+<script>
+import LeftNav from './components/common/leftNav'
+export default {
+  name: 'App',
+  components:{
+    LeftNav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.main{float: left;width: 95%;height: 100%;overflow: hidden;background-color: #f9fafc;}
+</style>
